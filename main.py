189836f544139c8b972b6f47ca554cb0c9d9ae77
marnixah <marnix@hagehoeve.com>
2022-02-22 00:40:56 +0100

    feat: cli

---
+++ main.py
@@ -0,0 +1,13 @@
+import click
+from parser import JapaneseAsmr
+
+@click.command()
+@click.argument('url')
+@click.argument('output', default="")
+def main(url, output):
+  japanese_asmr = JapaneseAsmr(url)
+  print(f"Downloading {japanese_asmr.title}")
+  japanese_asmr.render_video(output)
+
+if __name__ == '__main__':
+  main()
